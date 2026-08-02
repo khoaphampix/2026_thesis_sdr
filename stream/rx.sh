9366a3e2c15576396825136771bf0fcd72fad082
khoaphampix <khoapham.pix@gmail.com>
2026-08-02 22:27:43 +0930

    higher solution

--- stream/rx.sh
+++ stream/rx.sh
@@ -1,3 +1,27 @@
+# python3 pluto_video_rx_rtsp_arq.py \
+# --uri "usb:" \
+# --frequency 915000000 \
+# --sample-rate 2000000 \
+# --samples-per-bit 4 \
+# --payload-size 400 \
+# --rx-gain 30 \
+# --tx-gain -20 \
+# --ack-airtime 0.25 \
+# --final-ack-airtime 1.0 \
+# --turnaround-guard 0.02 \
+# --candidates-per-phase 16 \
+# --metric-threshold 0.35 \
+# --playback-prebuffer-bytes 4000 \
+# --rx-save two_pluto_camera_received.h265
+
+
+# --ack-airtime 0.18 \
+# --final-ack-airtime 0.80 \
+# --turnaround-guard 0.02
+
+
+
+# 320x180.h265
 python3 pluto_video_rx_rtsp_arq.py \
 --uri "usb:" \
 --frequency 915000000 \
@@ -11,5 +35,11 @@ python3 pluto_video_rx_rtsp_arq.py \
 --turnaround-guard 0.02 \
 --candidates-per-phase 16 \
 --metric-threshold 0.35 \
---playback-prebuffer-bytes 4000 \
---rx-save two_pluto_camera_received.h265
+--playback-prebuffer-bytes 6000 \
+# --playback-prebuffer-bytes 8000 \   # 426x240.h265
+# --playback-prebuffer-bytes 12000 \   # 640x360.h265
+--rx-save two_pluto_camera_received_320x180.h265
+
+
+
+
--- stream/tx.sh
+++ stream/tx.sh
@@ -1,15 +1,96 @@
-python3 pluto_video_rx_rtsp_arq.py \
+# python3 pluto_video_tx_rtsp_arq.py \
+# --uri "usb:" \
+# --duration 60 \
+# --video-size 256x144 \
+# --fps 2 \
+# --video-bitrate 2 \
+# --keyint-seconds 20 \
+# --encoder-preset veryfast \
+# --frequency 915000000 \
+# --sample-rate 2000000 \
+# --samples-per-bit 4 \
+# --payload-size 400 \
+# --tx-gain -20 \
+# --rx-gain 30 \
+# --data-airtime 0.20 \
+# --turnaround-guard 0.02 \
+# --ack-captures 12 \
+# --post-ack-guard 0.20 \
+# --retries 30 \
+# --tx-save two_pluto_camera_tx.h265
+
+
+# --ack-airtime 0.18 \
+# --final-ack-airtime 0.80 \
+# --turnaround-guard 0.02
+
+
+python3 pluto_video_tx_rtsp_arq.py \
 --uri "usb:" \
+--duration 60 \
+--video-size 320x180 \
+--fps 2 \
+--video-bitrate 3 \
+--keyint-seconds 15 \
+--encoder-preset veryfast \
 --frequency 915000000 \
 --sample-rate 2000000 \
 --samples-per-bit 4 \
 --payload-size 400 \
---rx-gain 30 \
 --tx-gain -20 \
---ack-airtime 0.25 \
---final-ack-airtime 1.0 \
+--rx-gain 30 \
+--data-airtime 0.20 \
 --turnaround-guard 0.02 \
---candidates-per-phase 16 \
---metric-threshold 0.35 \
---playback-prebuffer-bytes 4000 \
---rx-save two_pluto_camera_received.h265
+--ack-captures 12 \
+--post-ack-guard 0.20 \
+--retries 30 \
+--tx-save two_pluto_camera_tx_320x180.h265
+
+
+
+# 426x240
+
+# python3 pluto_video_tx_rtsp_arq.py \
+# --uri "usb:" \
+# --duration 60 \
+# --video-size 426x240 \
+# --fps 2 \
+# --video-bitrate 4 \
+# --keyint-seconds 15 \
+# --encoder-preset veryfast \
+# --frequency 915000000 \
+# --sample-rate 2000000 \
+# --samples-per-bit 4 \
+# --payload-size 400 \
+# --tx-gain -20 \
+# --rx-gain 30 \
+# --data-airtime 0.20 \
+# --turnaround-guard 0.02 \
+# --ack-captures 12 \
+# --post-ack-guard 0.20 \
+# --retries 30 \
+# --tx-save two_pluto_camera_tx_426x240.h265
+
+
+# 640x360
+
+# python3 pluto_video_tx_rtsp_arq.py \
+# --uri "usb:" \
+# --duration 60 \
+# --video-size 640x360 \
+# --fps 1 \
+# --video-bitrate 4 \
+# --keyint-seconds 15 \
+# --encoder-preset faster \
+# --frequency 915000000 \
+# --sample-rate 2000000 \
+# --samples-per-bit 4 \
+# --payload-size 400 \
+# --tx-gain -20 \
+# --rx-gain 30 \
+# --data-airtime 0.20 \
+# --turnaround-guard 0.02 \
+# --ack-captures 12 \
+# --post-ack-guard 0.20 \
+# --retries 30 \
+# --tx-save two_pluto_camera_tx_640x360.h265
